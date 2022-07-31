a8416ad73cff4d5ccc20e179334094c470feeae7
yanivys4 <yanivys4@gmail.com>
2022-07-31 18:13:54 +0300

    start creating bottom tab navigator

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,37 +1,63 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import AccountScreen from './AccountScreen';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import TabIcon from '../components/TabIcon';
+import ImageMap from '../assets/icons/ImagesMap';
 
 const Tab = createBottomTabNavigator();
 
 function Notifications() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Notifications!</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
 
 const HomeScreen = () => {
 
-    return (
-        <Tab.Navigator activeColor="#e91e63" style={{ backgroundColor: 'tomato' }} >
-          <Tab.Screen name="Account" component={AccountScreen} options={{
-              tabBarLabel: 'Account',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}/>
-              <Tab.Screen name="Notifications" component={Notifications} options={{
-              tabBarLabel: 'Notifications',
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons name="home" color={color} size={26} />
-              ),
-            }}/>
-        </Tab.Navigator>
-      );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabStyle,
+        ...styles.shadow
+      }}
+
+
+    >
+      <Tab.Screen name="Account" component={AccountScreen} options={{
+        tabBarIcon: ({ focused }) => TabIcon({ imageName:ImageMap.Home , title: 'Account', focused })
+      }}
+      />
+      <Tab.Screen name="Notifications" component={Notifications} options={{
+
+      }} />
+    </Tab.Navigator>
+  );
 }
 
+const styles = StyleSheet.create({
+  tabStyle: {
+    position: 'absolute',
+    bottom: 25,
+    left: 20,
+    right: 20,
+    elevation: 0,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    height: 90
+  },
+  shadow: {
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
+  }
+})
 export default HomeScreen;

e2c904fb20b21caf4bde57e4f335e2008c2c2f10
yanivys4 <yanivys4@gmail.com>
2022-10-19 14:18:00 +0300

    add assets to image map

--- src/assets/icons/ImagesMap.js
+++ src/assets/icons/ImagesMap.js
@@ -1,5 +1,7 @@
 export default {
     'Account': require('./Account.png'),
     'History': require('./History.png'),
-    'QR': require('./QR.png')
+    'QR': require('./QR.png'),
+    'Bill': require('./Bill.png'),
+    'Table': require('./Table.png')
 };

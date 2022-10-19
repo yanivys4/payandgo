8d5822d8508f0d55fec77555ac5d45a12906724b
yanivys4 <yanivys4@gmail.com>
2022-10-19 14:17:10 +0300

    add BillSlice to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import AuthReducer from './slices/AuthSlice';
+import BillReducer from './slices/BillSlice';
 
 export const store = configureStore({
   reducer: {
     auth:AuthReducer,
+    bill:BillReducer,
   },
 });

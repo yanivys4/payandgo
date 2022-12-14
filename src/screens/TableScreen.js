73e481505fdf44fc3acd62c88389640e5f56d6dc
yanivys4 <yanivys4@gmail.com>
2022-12-14 13:48:54 +0200

    fill table screen with content

--- src/screens/TableScreen.js
+++ src/screens/TableScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,StyleSheet} from 'react-native';
+import { setMealToDelete, deleteMealFromTable } from '../slices/TableSlice';
+import { addMeal } from '../slices/BillSlice';
+import { useSelector } from 'react-redux';
+import MealList from '../components/MealList';
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 
 const TableScreen = () =>{
+    const { tableMeals } = useSelector((state) => state.table);
+  
     return (
-        <View>
-            <Text>TableScreen</Text>
+        <View style={styles.container}>
+            <View style={styles.mealListContainerStyle}>
+                <MealList
+                    meals={tableMeals}
+                    pressIcon={faPlus}
+                    iconColor='green'
+                    itemToDeleteFunc={setMealToDelete}
+                    itemToAddFunc={addMeal}
+                    deleteItemFunc={deleteMealFromTable} />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    mealListContainerStyle: {
+        flex: 4
+
+    },
+    buttonContainerStyle: {
+        flex: 1,
+        marginTop: 10
+    }
+});
 
 export default TableScreen;

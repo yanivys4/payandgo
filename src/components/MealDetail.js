440d50540a863cda553fec4ba388eb38bb3d109f
yanivys4 <yanivys4@gmail.com>
2022-12-14 12:10:17 +0200

    change to swipable gesture

--- src/components/MealDetail.js
+++ src/components/MealDetail.js
@@ -1,54 +1,93 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { Card, CardSection } from './common';
-import { showModal } from '../slices/PopupDialogSlice';
-import {deleteMealFromBill,setMealToDelete} from '../slices/BillSlice';
+
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
-const MealDetail = ({ meal,icon }) => {
+const MealDetail = ({ meal, icon, pressAction,iconColor }) => {
 
     const { thumbnail_image, title, description, price, quantity } = meal;
     const dispatch = useDispatch();
 
-    return (
-        <Card>
-            <CardSection>
-                <View style={styles.quantityContainerStyle}>
-                    <Text style={styles.quantityTextStyle}>{`X${quantity}`}</Text>
-                </View>
-                <View style={styles.thumbnailImageContainerStyle} >
-                    <Image
-                        style={styles.thumbnailImageStyle}
-                        source={{ uri: thumbnail_image }}
-                    />
-                </View>
-                <View style={styles.headerContentStyle}>
-                    <Text style={styles.headerTitleStyle}>{title}</Text>
-                    <View>
-                        <Text style={styles.headerDescriptionStyle}>{description}</Text>
+    const swipeableRef = useRef(null);
+
+    const closeSwipeable = () => {
+        swipeableRef.current.close();
+    }
+
+    const rightSwipeActions = () => {
+
+        return (
+            <TouchableOpacity onPress={() => {
+                pressAction();
+                closeSwipeable();
+
+            }}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: iconColor,
+                        justifyContent: 'center',
+                        alignItems: 'flex-end',
+                    }}
+                >
+                    <View
+                        style={{
+                            color: 'white',
+                            paddingHorizontal: 10,
+                            fontWeight: '600',
+                            paddingHorizontal: 30,
+                            paddingVertical: 20,
+                        }}
+                    >
+                        <FontAwesomeIcon icon={icon} color='white' />
                     </View>
-                </View>
-                <View style={styles.priceContainerStyle} >
-                    <Text style={styles.priceTextStyle}>{`${price}$`}</Text>
-                </View>
-                <View style={styles.priceContainerStyle}>
-                    <TouchableOpacity onPress={() => {
-                        dispatch(setMealToDelete(title));
-                        if (quantity > 1) {
-                            dispatch(showModal({ quantity, title }));
-
-                        } else {
-                            dispatch(deleteMealFromBill(1));
-                        }
-                    }}>
-                        <FontAwesomeIcon icon={icon} color='#9e2b2b' />
-                    </TouchableOpacity>
 
                 </View>
-            </CardSection>
-        </Card>
+            </TouchableOpacity>
+
+        );
+    };
+    const showQuantity = (quantity) => {
+        if(quantity!=undefined){
+            return(
+                <View style={styles.quantityContainerStyle}>
+                            <Text style={styles.quantityTextStyle}>{`X${quantity}`}</Text>
+                        </View>
+            );
+        }
+    }
+    return (
+        <GestureHandlerRootView>
+            <Card>
+                <Swipeable ref={swipeableRef} renderRightActions={rightSwipeActions}>
+                    <CardSection>
+                        
+                        {showQuantity(quantity)}
+                        <View style={styles.thumbnailImageContainerStyle} >
+                            <Image
+                                style={styles.thumbnailImageStyle}
+                                source={{ uri: thumbnail_image }}
+                            />
+                        </View>
+                        <View style={styles.headerContentStyle}>
+                            <Text style={styles.headerTitleStyle}>{title}</Text>
+                            <View>
+                                <Text style={styles.headerDescriptionStyle}>{description}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.priceContainerStyle} >
+                            <Text style={styles.priceTextStyle}>{`${price}$`}</Text>
+                        </View>
+                    </CardSection>
+                </Swipeable>
+            </Card>
+        </GestureHandlerRootView>
+
     );
 }
 
@@ -61,9 +100,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginRight: 10,
-        // marginLeft: 5,
-        // borderWidth: 1,
-        // borderColor: 'green',
         flex: 1
     },
     priceContainerStyle: {
@@ -71,29 +107,22 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginRight: 10,
         marginLeft: 5,
-        // borderWidth: 1,
-        // borderColor: 'green',
         flex: 1
     },
     quantityContainerStyle: {
-        // alignItems: 'center',
         justifyContent: 'center',
         marginRight: 10,
         marginLeft: 10,
-        // borderWidth: 1,
-        // borderColor: 'green',
 
     },
     quantityTextStyle: {
         fontSize: 14,
         color: '#9e2b2b',
-        fontWeight:'bold'
+        fontWeight: 'bold'
     },
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around',
-        // borderWidth: 1,
-        // borderColor: 'red',
         flex: 4
     },
     headerTitleStyle: {

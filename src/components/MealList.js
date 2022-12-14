6149a59bf1c3879cd22ec99800a05aa854085875
yanivys4 <yanivys4@gmail.com>
2022-12-14 13:44:55 +0200

    add and remove items in meal list

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,34 +1,44 @@
-import React, { useEffect } from 'react';
-import { View, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList,ActivityIndicator } from 'react-native';
 import MealDetail from '../components/MealDetail';
 import { fetchMeals } from '../actions/BillActions';
 import { useDispatch } from 'react-redux';
 
-const MealList = ({meals,pressIcon}) => {
+
+const MealList = ({ meals, pressIcon, iconColor, itemToDeleteFunc, itemToAddFunc, deleteItemFunc }) => {
     const dispatch = useDispatch();
+    
     // useEffect(() => {
-    //     dispatch(fetchMeals());
+    //     // dispatch(fetchMeals());
     // },[]);
 
     const renderItem = ({ item }) => (
-        <MealDetail meal={item} icon={pressIcon} />
-    );
+        <MealDetail meal={item} icon={pressIcon} iconColor={iconColor} pressAction={() => {
+            dispatch(itemToDeleteFunc(item.description));
+            dispatch(itemToAddFunc(
+                {title:item.title,
+                description:item.description,
+                price:item.price,
+                thumbnail_image:item.thumbnail_image,
+                id:item.id
+                }));
+            dispatch(deleteItemFunc());
 
-    const renderMeals = () => {
-        if (meals) {
+        }} />
+    );
+    const renderMeals = ()=>{
+        if(meals){
             return (
                 <FlatList
                     data={meals}
                     renderItem={renderItem}
-                    keyExtractor={meal => meal.title}
+                    keyExtractor={item => item.id}
                 />
             );
-        } else {
+        }else{
             return <ActivityIndicator size='large' />;
         }
-
-    };
-
+    }    
     return (
         <View style={{}}>
             {renderMeals()}
